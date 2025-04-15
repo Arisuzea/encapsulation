6b5e34d165fa584cca5660b669af353ab84e2142
Arisuzea <grldcondezz18@gmail.com>
2025-04-15 12:31:14 +0800

    first commit

---
+++ src/main/java/encapsulation/inheritance/activity/Main.java
@@ -0,0 +1,123 @@
+package encapsulation.inheritance.activity;
+
+import java.util.Scanner;
+
+// Galit ako sa typing
+// kaya po ganyarn sya, tinks
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        
+// Create instances of helper classes
+        productScanners scannerHelper = new productScanners();
+        displayProducts displayHelper = new displayProducts();
+
+// Get product info from user
+        DiscountedProduct product = scannerHelper.productScanner(scanner);
+
+// Display product details
+        displayHelper.displayProductDetails(product);
+    }
+}
+
+
+class DiscountedProduct extends Product {
+    private double discountRate;
+
+// Call parent class values before discounting
+    public DiscountedProduct(String productName, String productCode, double price, double discountRate) {
+        super(productName, productCode, price);
+        this.discountRate = discountRate;
+    }
+
+// Getter
+    public double getDiscountRate() {
+        return discountRate;
+    }
+
+// Setter
+    public void setDiscountRate(double discountRate) {
+        this.discountRate = discountRate;
+    }
+
+// Actual discounting and updating price
+    @Override
+    public double getPrice() {
+        double originalPrice = super.getPrice();
+        return originalPrice - (originalPrice * discountRate / 100);
+    }
+}
+
+
+class Product {
+    private String productName;
+    private String productCode;
+    private double price;
+
+// Constructor
+    public Product(String productName, String productCode, double price) {
+        this.productName = productName;
+        this.productCode = productCode;
+        this.price = price;
+    }
+
+
+
+// Getters
+    public String getProductName() {
+        return productName;
+    }
+
+    public String getProductCode() {
+        return productCode;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+
+
+// Setters
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public void setProductCode(String productCode) {
+        this.productCode = productCode;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+}
+
+class productScanners {
+    public DiscountedProduct productScanner(Scanner scanner) {
+        System.out.print("Enter product name: ");
+        String name = scanner.nextLine();
+
+        System.out.print("Enter product code: ");
+        String code = scanner.nextLine();
+
+        System.out.print("Enter product price: ");
+        double price = scanner.nextDouble();
+
+        System.out.print("Enter discount rate (in percentage): ");
+        double discount = scanner.nextDouble();
+        scanner.nextLine();
+
+        return new DiscountedProduct(name, code, price, discount);
+    }
+}
+
+class displayProducts {
+    public void displayProductDetails(DiscountedProduct product) {
+        System.out.println("Product Name: " + product.getProductName());
+        System.out.println("Product Code: " + product.getProductCode());
+        System.out.println("Original Price: " + product.getPrice());
+        System.out.println("Discounted Price: " + product.getPrice());
+    }
+}
